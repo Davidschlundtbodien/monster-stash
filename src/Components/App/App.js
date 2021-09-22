b411c42e7abda81b6627a1bf2f4dc882f8ecf417
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-09-22 14:07:06 -0600

    Add cr list component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from '../Navbar/Navbar'
+import ChallengeRatingList from '../ChallengeRatingList/ChallengeRatingList'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <ChallengeRatingList />
     </div>
   );
 }
---
+++ src/Components/ChallengeRatingList/ChallengeRatingList.js
@@ -0,0 +1,14 @@
+import './ChallengeRatingList.css'
+
+const ChallengeRatingList = () => {
+  return(
+    <article>
+      <p>CR 1</p>
+      <p>CR 2</p>
+      <p>CR 3</p>
+      <p>CR 4</p>
+    </article>
+  )
+}
+
+export default ChallengeRatingList;
