968601efb8d3825d90b91d85a5a2107689f4c9f2
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-09-22 14:32:23 -0600

    Add monster list component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from '../Navbar/Navbar'
 import ChallengeRatingList from '../ChallengeRatingList/ChallengeRatingList'
+import MonsterList from '../MonsterList/MonsterList'
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
       <ChallengeRatingList />
+      <MonsterList />
     </div>
   );
 }
