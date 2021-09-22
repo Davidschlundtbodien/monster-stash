20f6ef378df7fa3c6bf24dba54ee8c9f1c80d057
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-09-22 14:45:38 -0600

    Add monster info component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from '../Navbar/Navbar'
 import ChallengeRatingList from '../ChallengeRatingList/ChallengeRatingList'
 import MonsterList from '../MonsterList/MonsterList'
+import MonsterInfo from '../MonsterInfo/MonsterInfo'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <ChallengeRatingList />
       <MonsterList />
+      <MonsterInfo />
     </div>
   );
 }
