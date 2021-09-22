fdca30e8966be24b00a622ae96b916a64c4f812d
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-09-22 15:04:02 -0600

    Add react router switch and basic routes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,14 +3,23 @@ import Navbar from '../Navbar/Navbar'
 import ChallengeRatingList from '../ChallengeRatingList/ChallengeRatingList'
 import MonsterList from '../MonsterList/MonsterList'
 import MonsterInfo from '../MonsterInfo/MonsterInfo'
+import { Switch, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
-      <ChallengeRatingList />
-      <MonsterList />
-      <MonsterInfo />
+      <Switch>
+        <Route exact path="/">
+          <ChallengeRatingList />
+        </Route>
+        <Route exact path="/monsters">
+          <MonsterList />
+        </Route>
+        <Route exact path="/monsters/:name">
+          <MonsterInfo />
+        </Route>
+      </Switch>
     </div>
   );
 }
