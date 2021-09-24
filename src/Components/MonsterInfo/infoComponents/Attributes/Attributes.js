d85a41527089a7ae8d549f2e1be261a3dc30ef2f
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-09-24 17:23:32 -0600

    Add styling to attributes

--- src/Components/MonsterInfo/infoComponents/Attributes/Attributes.js
+++ src/Components/MonsterInfo/infoComponents/Attributes/Attributes.js
@@ -3,14 +3,16 @@ import './Attributes.css'
 const Attributes = ({str, dex, con, int, wis, cha}) => {
 
   return(
-    <section>
-      <p>Attributes</p>
-      <p>Strength || {str}</p>
-      <p>Dexterity || {dex}</p>
-      <p>Constitution || {con}</p>
-      <p>Intelligence || {int}</p>
-      <p>Wisdom || {wis}</p>
-      <p>Charisma || {cha}</p>
+    <section className="attr-container">
+      <p className="attr-header">Attributes</p>
+      <div className="attr-content">
+        <p>Strength || {str}</p>
+        <p>Dexterity || {dex}</p>
+        <p>Constitution || {con}</p>
+        <p>Intelligence || {int}</p>
+        <p>Wisdom || {wis}</p>
+        <p>Charisma || {cha}</p>
+      </div>
     </section>
   )
 }
