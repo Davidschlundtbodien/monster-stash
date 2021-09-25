ad7dca07fc0cf451fe5e9890af76e69990a34b33
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-09-25 12:19:28 -0600

    Add custom intercept and stub for monsterlist requests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,23 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('getMonsterList', (cr) => {
+  cy.intercept('GET', `https://www.dnd5eapi.co/api/monsters?challenge_rating=${cr}`, {
+    body: {
+      "results": [
+        {
+          "index": "awakened-shrub",
+          "name": "Awakened Shrub",
+          "url": "/api/monsters/awakened-shrub"
+        },
+        {
+          "index": "baboon",
+          "name": "Baboon",
+          "url": "/api/monsters/baboon"
+        },
+        {
+          "index": "badger",
+          "name": "Badger",
+          "url": "/api/monsters/badger"
+        }
+      ]
+    }
+  })
+})
