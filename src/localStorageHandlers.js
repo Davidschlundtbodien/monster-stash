39faddcda1bad06c5c67495d6f7ad702746877f1
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-09-25 15:54:54 -0600

    Add delete from storage function

--- src/localStorageHandlers.js
+++ src/localStorageHandlers.js
@@ -5,16 +5,25 @@ export const setToLocal = (monster) => {
   if(isRepeat) {
     return
   }
-  
+
   const newMonsterList = [...monsterStorage, monster]
   const listString = JSON.stringify(newMonsterList)
   localStorage.setItem('favorites', listString)
 }
 
 export const checkStorage = () => {
+
   if(!localStorage.getItem('favorites')) {
     localStorage.setItem('favorites', '[]')
   }
+
   const response = localStorage.getItem('favorites')
   return JSON.parse(response)
+}
+
+export const deleteFromStorage = (monsterIndex) => {
+  const monsterStorage = checkStorage()
+  const newList = monsterStorage.filter(e => e.index !== monsterIndex)
+  const listString = JSON.stringify(newList)
+  localStorage.setItem('favorites', listString)
 }
