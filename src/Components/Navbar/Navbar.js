0b2c4b014ea72ff4086a567a96279d67e85ac677
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-09-25 13:38:16 -0600

    Add links to nav bar component

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,11 +1,12 @@
 import './Navbar.css'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return(
     <nav>
-      <h1>Monster Stash</h1>
+      <h1><Link to="/" className="link">Monster Stash</Link></h1>
       <div className='nav-links'>
-        <p>Favorites</p>
+        <p><Link to='/favorites' className="link">Favorites</Link></p>
         <p>Encounters</p>
       </div>
     </nav>
