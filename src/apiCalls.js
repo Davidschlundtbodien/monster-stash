a43de1834cb6d14afc134ef29b1ba32c73b43cac
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-09-27 12:28:59 -0600

    Add error handling to requests

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,11 +1,19 @@
 export const fetchMonsters = (challengeRating) => {
   return fetch(`https://www.dnd5eapi.co/api/monsters?challenge_rating=${challengeRating}`)
-  .then(response => response.json())
-  .catch(error => console.log(error))
+  .then(response => checkResponse(response))
 }
 
 export const fetchMonsterInfo = (monsterIndex) => {
   return fetch(`https://www.dnd5eapi.co/api/monsters/${monsterIndex}`)
-  .then(response => response.json())
-  .catch(error => console.log(error))
+  .then(response => checkResponse(response))
+  
+}
+
+
+const checkResponse = (response) => {
+  if (response.ok) {
+    return response.json()
+  } else {
+    return Promise.reject(`error ${response.status}`)
+  }
 }
